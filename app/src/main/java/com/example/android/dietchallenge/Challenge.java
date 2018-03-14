af6d69791e9ef0e6c4d35d80dd59bc4ef5f2ee68
Ivana Leban <ivanal123@gmail.com>
2018-03-14 18:02:58 +0100

    1--3

--- app/src/main/java/com/example/android/dietchallenge/Challenge.java
+++ app/src/main/java/com/example/android/dietchallenge/Challenge.java
@@ -14,7 +14,8 @@ import android.widget.Toast;
 public class Challenge extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     //declare both spinner from xml
-    Spinner spinner1, spinner2;
+    Spinner spinner1;
+    Spinner spinner2;
     //define adapter
     ArrayAdapter adapter;
 
@@ -27,32 +28,24 @@ public class Challenge extends AppCompatActivity implements AdapterView.OnItemSe
     int scoreTeamA = 0;
     int scoreTeamB = 0;
 
-
     String name1 = "";
     String name2 = "";
 
-
-    TextView firstName, secondName, context, message1;
-
-
-
+    TextView firstName;
+    TextView secondName;
+    TextView context;
+    TextView message1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge);
 
-
-
-
-
             message1 = (TextView) findViewById(R.id.newMessage);
             message1.setText("We are going to try to find the best recepies just for you! While you wait you can begin the Challege! Enjoy!");
 
-
         // Create an ArrayAdapter using the string array and a default spinner layout
         adapter = ArrayAdapter.createFromResource(this, R.array.name_list, R.layout.support_simple_spinner_dropdown_item);
-
 
         //define spinner
         spinner1 = (Spinner) findViewById(R.id.spinner1);
@@ -65,14 +58,10 @@ public class Challenge extends AppCompatActivity implements AdapterView.OnItemSe
         spinner2.setAdapter(adapter);
         spinner2.setOnItemSelectedListener(Challenge.this);
 
-
-
     }
     @Override
     protected void onSaveInstanceState (Bundle outState){
         super.onSaveInstanceState(outState);
-
-
 
         outState.putInt("score", score1);
         outState.putInt("score", score2);
@@ -108,13 +97,11 @@ public class Challenge extends AppCompatActivity implements AdapterView.OnItemSe
 
         displayForTeamA (score3);
 
-
     }
 
     public void input4 (View v){
 
         displayForTeamA (score4);
-
 
     }
 
@@ -134,16 +121,13 @@ public class Challenge extends AppCompatActivity implements AdapterView.OnItemSe
 
         displayForTeamB (score3);
 
-
     }
 
     public void input8 (View v){
 
         displayForTeamB (score4);
 
-
     }
-
 
     public void enter (View v){
 
@@ -165,17 +149,11 @@ public class Challenge extends AppCompatActivity implements AdapterView.OnItemSe
             count = 0;
         }
 
-
     }
-
-
 
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
         parent.getItemAtPosition(pos);
-
-
-
 
     }
 
@@ -183,28 +161,21 @@ public class Challenge extends AppCompatActivity implements AdapterView.OnItemSe
         // Another interface callback
     }
 
-
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.view_score1);
         scoreView.setText(String.valueOf(score));
         scoreTeamA = score;
 
-
     }
-
-
-
 
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.view_score2);
         scoreView.setText(String.valueOf(score));
         scoreTeamB = score;
 
-
     }
 
     public void show (View view){
-
 
         TextView firstName = (TextView)spinner1.getSelectedView();
         String name1 = spinner1.getSelectedItem().toString();
@@ -212,9 +183,7 @@ public class Challenge extends AppCompatActivity implements AdapterView.OnItemSe
         TextView secondName = (TextView)spinner2.getSelectedView();
         String name2 = spinner2.getSelectedItem().toString();
 
-
         if (scoreTeamA == score1 && scoreTeamB == score1 || scoreTeamA == score2 && scoreTeamB == score2) {
-
 
             TextView context = (TextView) findViewById(R.id.context);
             //set text to textView
@@ -242,8 +211,6 @@ public class Challenge extends AppCompatActivity implements AdapterView.OnItemSe
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(message, text, duration);
         toast.show();
-
     }
-
 
 }
